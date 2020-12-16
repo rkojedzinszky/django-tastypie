a0696b21ab92aade5ba5ac146bf651adfd577a5b
Richard Kojedzinszky <richard@kojedz.in>
2020-12-16 10:17:00 +0100

    Handle skipped m2m fields in patch method

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -2515,6 +2515,9 @@ class BaseModelResource(Resource):
             if field_object.readonly:
                 continue
 
+            if bundle.data.get(field_name) is None:
+                continue
+
             # Get the manager.
             related_mngr = None
 
